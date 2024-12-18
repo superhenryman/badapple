24484294b58b321af44ad96e0994cb4b74f9d36e
superhenryman <121818148+superhenryman@users.noreply.github.com>
2024-12-18 17:37:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bad Apple!</title>
+    <style>
+        body {
+            font-family:Arial, Helvetica, sans-serif;
+            
+        }
+    </style>
+</head>
+<body>
+    <h1>Click the video below!</h1>
+    <video controls>
+        <source src="badapple.mp4">
+    </video>
+    <p><em>please work...</em></p>
+</body>
+</html>
